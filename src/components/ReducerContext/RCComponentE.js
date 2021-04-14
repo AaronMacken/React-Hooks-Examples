036dcf0dca1bd246b06b7c8eeca5dc66e376e8e3
Aaron Macken <aaron.a.macken@gmail.com>
2021-04-14 08:53:47 -0500

    useContext with useReducer example

---
+++ src/components/ReducerContext/RCComponentE.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ComponentF from './RCComponentF';
+
+function ComponentE() {
+    return (
+        <div>
+            <ComponentF />
+        </div>
+    )
+}
+
+export default ComponentE;
